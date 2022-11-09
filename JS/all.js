46f208f72a934321524369fa698df1641f209485
fin <Polly820219@gmail.com>
2022-11-09 23:57:21 +0800

    view banner完成

--- JS/all.js
+++ JS/all.js
@@ -1,9 +1,36 @@
-gsap.registerPlugin(ScrollTrigger, TextPlugin);
 
-// 文字無限循環
-gsap.to(".loop", {
-  xPercent: "-50",
-  ease: "none",
-  duration: 10,
-  repeat: -1,
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  
+  gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+  const timeline = gsap.timeline();
+
+  // 文字無限循環
+
+
+  // timeline.fromTo(".banner .txt_md", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+  timeline.from(".hero .txt_md" , {y : 200 ,duration:1});
+
+  timeline.from(".loop1", {xPercent: "-50",ease: "none",duration: 2,repeat: -1});
+
+  timeline.fromTo(".group1", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+  timeline.fromTo(".group2", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+  timeline.fromTo(".group3", {autoAlpha: 0}, {autoAlpha: 1, duration: 1});
+
+  var bannerWidth = parseInt($(".banner").css("width"));
+  var imgWidth = parseInt($(".loop2 img").css("width"));
+
+  timeline.to(".loop2", {x:() => { return (bannerWidth-imgWidth)} ,ease: "none",duration: 2});
+  timeline.from(".loop3", {x:() => { return (bannerWidth-imgWidth)},ease: "none",duration: 2}, "<");
+
+
+});
+
+$("#hamburger").on('click', () => {
+  $(".hamBar").toggleClass('active');
+});
+$(".xmark").on('click', () => {
+  $(".hamBar").toggleClass('active');
 });
