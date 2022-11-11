349b280d3c81acd5ba6cceb810acf5fc4f03e48f
fin <Polly820219@gmail.com>
2022-11-11 22:30:15 +0800

    完成

--- JS/all.js
+++ JS/all.js
@@ -80,40 +80,34 @@
         timeline.from('.banner .img_leftHand', {left: 120, opacity:0 ,duration:3},"<");
       };
     },
+  });
+
+  //carArea
+  const carArea = document.querySelector(".carArea")
+  var carAreaWidth = parseInt($(".carArea").css("width"));
+  var vectorWidth = parseInt($(".carArea .img_vector").css("width"));
+  var carWidth = parseInt($(".carArea .img_car").css("width"));
+
+  ScrollTrigger.create({
+    trigger: carArea,
+    markers: true,
+
+    //向下滾動進入start點時觸發callback
+    onEnter: function () {
+      gsap.to(".img_car",{x :() =>{return (carWidth + vectorWidth) - carAreaWidth},duration:3});
+    },
 
     //向下滾動超過end點時觸發callback
     onLeave: function () {
-      gsap.set(".banner .img_title" ,  { autoAlpha: 0 });
-      gsap.set(".banner .txt_title" ,  { autoAlpha: 0 });
-      gsap.set(".banner .img_front" ,  { autoAlpha: 0 });
-      gsap.set(".banner .img_uxdesign" ,  { autoAlpha: 0 });
-      gsap.set(".banner .img_rightHand" ,  { autoAlpha: 0 });
-      gsap.set(".banner .img_leftHand" ,  { autoAlpha: 0 });
-      // gsap.set(".loop2" ,  { transform: () => {return 'translate' + (imgWidth - wrapWidth, 0)} });
-      // gsap.set(".loop3" ,  { transform: () => {return 'translate' + (imgWidth - wrapWidth, 0)} });
+      gsap.set(".img_car" ,  {x :() =>{return carAreaWidth - (carWidth + vectorWidth)}});
     }, 
 
     //向上滾動超過end點時觸發（回滾時觸發）callback
     onEnterBack: function () {
-      timeline.to(".loop2", {x:() => { return (wrapWidth-imgWidth)} ,ease: "none",duration: 2});
-      timeline.from(".loop3", {x:() => { return (wrapWidth-imgWidth)},ease: "none",duration: 2}, "<");
-      timeline.from('.banner .img_title', {width:0, opacity:0 ,duration:3},"<");
-      timeline.from('.banner .txt_title', { opacity:0 ,duration:3},"<");
-      timeline.from('.banner .img_front', {width:0,top: -350,left: 500, opacity:0 ,duration:3},"<");
-      timeline.from('.banner .img_uxdesign', {width:0,top: 300,left: -110, opacity:0 ,duration:3},"<");
-
-      if (wrapWidth < padWidth){
-        timeline.from('.banner .img_rightHand', {top: 100, opacity:0 ,duration:3},"<");
-        timeline.from('.banner .img_leftHand', {top: -100, opacity:0 ,duration:3},"<");
-      }else{
-        timeline.from('.banner .img_rightHand', {left: -120, opacity:0 ,duration:3},"<");
-        timeline.from('.banner .img_leftHand', {left: 120, opacity:0 ,duration:3},"<");
-      };
+      gsap.to(".img_car",{x :() =>{return (carWidth + vectorWidth) - carAreaWidth},duration:3});
     },
 
   });
-
-  
 
 
 $("#hamburger").on('click', () => {
